4102592673caec3773fbeca714816a30687dccf4
sahil <sahilmadan0805@gmail.com>
2024-03-30 00:18:07 +0530

    g

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './components/Home/Home';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './components/Home/Home'
 import Services from "./components/Services/Services";
 import AboutUs from "./components/AboutUs/AboutUs";
 import ContactForm from './components/Contact/Contact';
@@ -12,24 +12,56 @@ import Service4 from './components/servicepages/service4';
 import Service5 from './components/servicepages/service5';
 
 function App() {
+    let router = createBrowserRouter([
+        {
+            path: '/global-trader',
+            element: <RootLayout />,
+            errorElement: <ErrorRoute />,
+            children: [
+                {
+                    path: '',
+                    element: <Home />
+                },
+                {
+                    path: 'services',
+                    element: <Services />
+                },
+                {
+                    path: 'aboutus',
+                    element: <AboutUs />
+                },
+                {
+                    path: 'contact',
+                    element: <ContactForm />
+                },
+                {
+                    path: 'service1',
+                    element: <Service1 />
+                },
+                {
+                    path: 'service2',
+                    element: <Service3 />
+                },
+                {
+                    path: 'service3',
+                    element: <Service3 />
+                },
+                {
+                    path: 'service4',
+                    element: <Service4 />
+                },
+                {
+                    path: 'service5',
+                    element: <Service5 />
+                }
+            ]
+        }
+    ]);
     return (
-        <BrowserRouter>
-            <RootLayout>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="services" element={<Services />} />
-                    <Route path="aboutus" element={<AboutUs />} />
-                    <Route path="contact" element={<ContactForm />} />
-                    <Route path="service1" element={<Service1 />} />
-                    <Route path="service2" element={<Service2 />} />
-                    <Route path="service3" element={<Service3 />} />
-                    <Route path="service4" element={<Service4 />} />
-                    <Route path="service5" element={<Service5 />} />
-                    <Route path="*" element={<ErrorRoute />} />
-                </Routes>
-            </RootLayout>
-        </BrowserRouter>
-    );
+        <div>
+            <RouterProvider router={router} />
+        </div>
+    )
 }
 
 export default App;
